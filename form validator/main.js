901247e18e7415c7954c848df16dd625fdb4c833
LauriMatias <laurimatias.reponen@gmail.com>
2022-11-19 19:06:57 +0200

    Small tweaks to the html to make it more pleasing, finished most of the css (will show password info while hovering over info image in the future), started the actual form validation functionality on main.js

---
+++ form validator/main.js
@@ -0,0 +1,25 @@
+const form = document.getElementById('form');
+const userName = document.getElementById('userName');
+const email = document.getElementById('email');
+const pw = document.getElementById('pw');
+const rePw = document.getElementById('rePw');
+
+function valForm() {
+
+};
+
+function valUserName() {
+
+};
+
+function valEmail() {
+
+};
+
+function valPw() {
+
+};
+
+function valRePw() {
+
+};
